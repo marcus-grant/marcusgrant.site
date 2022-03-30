9a36b2053a8e51bc33dc6b97ae35d3de82ea35d7
Marcus Grant <marcusfg@gmail.com>
2022-03-30 21:13:11 +0200

    Clear repository before change to frontity+wp

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [require('@tailwindcss/typography')],
-  purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
-  variants: {},
-  theme: {
-    extend: {},
-  },
-  darkMode: false, 
-};
